578f430f7f0a01f8994e0772f429c7f3462d7a2f
Aline Borges <ap.borges@live.com>
2019-10-26 16:59:55 -0300

    Change cdz image to png, add classes to tags, add functions to change theme

--- script.js
+++ script.js
@@ -1,73 +1,17 @@
-let removeDbz = document.querySelector("body").classList.remove('dbz')
-let removeBnh = document.querySelector("body").classList.remove('bnh')
-let removeCdz = document.querySelector("body").classList.remove('cdz')
-let removeNar = document.querySelector("body").classList.remove('nar')
-let removeFma = document.querySelector("body").classList.remove('fma')
-let removeBle = document.querySelector("body").classList.remove('ble')
-let removeYyh = document.querySelector("body").classList.remove('yyh')
-let removeRuk = document.querySelector("body").classList.remove('ruk')
-let removeNnt = document.querySelector("body").classList.remove('nnt')
+const body = document.querySelector('body')
+const avatar = document.querySelector('.user')
+const themeList = ['dbz', 'bnh', 'cdz', 'nar', 'fma', 'ble', 'yyh', 'ruk', 'nnt']
 
-let removeList = [removeDbz,removeBnh,removeCdz,removeNar,removeFma,removeBle,removeYyh,removeRuk,removeNnt]
-
-let addDbz = document.querySelector("body").classList.add('dbz')
-let addBnh = document.querySelector("body").classList.add('bnh')
-let addCdz = document.querySelector("body").classList.add('cdz')
-let addNar = document.querySelector("body").classList.add('nar')
-let addFma = document.querySelector("body").classList.add('fma')
-let addBle = document.querySelector("body").classList.add('ble')
-let addYyh = document.querySelector("body").classList.add('yyh')
-let addRuk = document.querySelector("body").classList.add('ruk')
-let addNnt = document.querySelector("body").classList.add('nnt')
-
-
-function removeAll(){
-    return removeList
+function removeAll()
+{
+    themeList.forEach(function(theme) {
+        body.classList.remove(theme)
+    })
 }
 
-
-function Dbz(){
+function changeTheme(theme)
+{
     removeAll()
-    return addDbz
-}
-
-function Bnh(){
-    removeAll()
-    return addBnh
-    
-}
-
-function Cdz(){
-    removeAll()
-    return addCdz
-}
-
-function Nar(){
-    removeAll()
-    return addNar
-}
-
-function Fma(){
-    removeAll()
-    return addFma
-}
-
-function Ble(){
-    removeAll()
-    return addBle
-}
-
-function Yyh(){
-    removeAll()
-    return addYyh
-}
-
-function Ruk(){
-    removeAll()
-    return addRuk
-}
-
-function Nnt(){
-    removeAll()
-    return addNnt
+    body.classList.add(theme)
+    avatar.setAttribute('src', 'img/' + theme + '.png')
 }
